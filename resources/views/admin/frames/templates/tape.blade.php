a3e7beb39560c7e7c325e1631a4efd0ea3afcddf
Naura Khoirunnisa <nauranisa1204@gmail.com>
2025-06-18 12:26:53 +0700

    Revert "frame"
    This reverts commit 7c941da4e2f2e5c6d98679356f71f007dd5b777f.

--- resources/views/admin/frames/templates/tape.blade.php
+++ resources/views/admin/frames/templates/tape.blade.php
@@ -1,14 +1,14 @@
-<div class="w-full h-full relative overflow-hidden shadow-[0_0_20px_rgba(0,0,0,0.3)]">
-     <img src="{{ asset('tape.png') }}" 
-         class="absolute top-0 left-0 opacity-80 z-0 pointer-events-none" />
-    <div class="absolute top-[20px] left-[10px] w-[calc(100%-20px)] h-[120px] z-10" data-photo-index="0">
+<div class="w-full h-full relative overflow-hidden shadow-[0_0_20px_rgba(0,0,0,0.3)]"
+    style="background-image: url('{{ asset('pita.png') }}'); background-size: cover; background-position: center;">
+
+    <div class="absolute top-[20px] left-[10px] w-[calc(100%-20px)] h-[120px]" data-photo-index="0">
         <div class="photo-slot">
             <img id="photo1" src="" class="w-full h-full object-cover">
             <button class="retake-button absolute top-1 right-1 text-lg" data-index="0" data-has-photo="false">⟲</button>
         </div>
     </div>
 
-    <div class="absolute top-[150px] left-[10px] w-[calc(100%-20px)] h-[120px] z-10" data-photo-index="1">
+    <div class="absolute top-[150px] left-[10px] w-[calc(100%-20px)] h-[120px]" data-photo-index="1">
         <div class="photo-slot">
             <img id="photo2" src="" class="w-full h-full object-cover">
             <button class="retake-button absolute top-1 right-1 text-lg" data-index="1"
@@ -16,7 +16,7 @@
         </div>
     </div>
 
-    <div class="absolute top-[280px] left-[10px] w-[calc(100%-20px)] h-[120px] z-10" data-photo-index="2">
+    <div class="absolute top-[280px] left-[10px] w-[calc(100%-20px)] h-[120px]" data-photo-index="2">
         <div class="photo-slot">
             <img id="photo3" src="" class="w-full h-full object-cover">
             <button class="retake-button absolute top-1 right-1 text-lg" data-index="2"
